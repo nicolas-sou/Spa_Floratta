985ba772e06f217ad802b8dd89e7b8b745bd5b07
Nicolas Sousa <nicollassouza141@gmail.com>
2024-03-21 19:44:46 -0300

    Parte 2

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 AOS.init();
 
-document.addEventListener('DOMContentLoaded', function(){
-    const buttons = document.querySelectorAll('[data-tab-button]');
-    
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', function(botao){
-            const abaAlvo = botao.target.dataset.tabButton;
-            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
-            escondetodasAbas();
-            aba.classList.add('customer__grid--is-active');
-        })
-    }
-})
-
-function escondetodasAbas(){
-    const tabsContainer = document.querySelectorAll('[data-tab-id]');
-
-    for(let i = 0; i < tabsContainer.length; i++) {
-        tabsContainer[i].classList.remove('customer__grid--is-active');
-    }
-}
+document.addEventListener("DOMContentLoaded", function () {
+    // Adiciona o evento de clique para links no navbar
+    $('a[href^="#Formulario"]').on('click', function (event) {
+        var target = $($(this).attr('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+});
